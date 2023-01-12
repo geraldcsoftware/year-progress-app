d92b3ecaa3c0723c66c25ebc07f88d0a78670f50
Gerald Chifanzwa <g.chifanzwa@theahoyapp.com>
2023-01-12 16:11:43 +0200

    added header with dummy icon and nav link to home

--- apps/year-progress-app/src/app/app.module.ts
+++ apps/year-progress-app/src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { NavModule } from './nav/nav.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, AppRoutingModule, NavModule],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ apps/year-progress-app/src/app/nav/header/header.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'year-progress-header',
+  templateUrl: './header.component.html',
+  styles: [],
+})
+export class HeaderComponent {}
