bd1a4f3014590b2cad1ec81adfab1eeec79a0b07
Gerald Chifanzwa <g.chifanzwa@theahoyapp.com>
2023-01-12 15:02:13 +0200

    Initial commit

---
+++ apps/year-progress-app/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'year-progress-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'year-progress-app';
+}
